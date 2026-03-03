4a531e255fc53bf7333e329b88c3c3a041ae3cfb
TrickyBAM <163210777+TrickyBAM@users.noreply.github.com>
2026-03-03 14:14:42 -0800

    Add team member data

---
+++ src/data/team.js
@@ -0,0 +1,38 @@
+// Team member data for Demand Local
+const PHOTO_BASE = 'https://www.demandlocal.com/wp-content/uploads/2025/09/';
+
+const team = [
+  { name: 'Ray Bhatia', title: 'CEO & Co-Founder', photo: `${PHOTO_BASE}Ray-Bhatia.png`, tier: 'executive' },
+  { name: 'Mike Machado', title: 'Chief Revenue Officer & Co-Founder', photo: `${PHOTO_BASE}Mike-Machado.png`, tier: 'executive' },
+  { name: 'Brian Schrader', title: 'Chief Product Officer', photo: `${PHOTO_BASE}Brian-Schrader.png`, tier: 'executive' },
+  { name: 'Caitlin Cowley', title: 'SVP, Growth & Strategy', photo: `${PHOTO_BASE}Caitlin-Cowley.png`, tier: 'vp' },
+  { name: 'Christian Ferrer', title: 'VP, Automotive Sales & Marketing', photo: `${PHOTO_BASE}Christian-Ferrer.png`, tier: 'vp' },
+  { name: 'Rob Schack', title: 'VP, Agency Development', photo: `${PHOTO_BASE}Rob-Schack.png`, tier: 'vp' },
+  { name: 'Lachlan Maclean', title: 'VP, Programmatic', photo: `${PHOTO_BASE}Lachlan-Maclean.png`, tier: 'vp' },
+  { name: 'Tiffany Hagwood', title: 'VP of Account Operations', photo: `${PHOTO_BASE}Tiffany-Hagwood.png`, tier: 'vp' },
+  { name: 'Kass Dawson', title: 'VP of Marketing', photo: `${PHOTO_BASE}Kass-Dawson.png`, tier: 'vp' },
+  { name: 'Lauren Richter', title: 'Executive Director of Sales', photo: `${PHOTO_BASE}Lauren-Richter.png`, tier: 'director' },
+  { name: 'Erica Lindo', title: 'Sr. Director of Agency Partnerships', photo: `${PHOTO_BASE}Erica-Lindo.png`, tier: 'director' },
+  { name: 'Valen Murdock', title: 'Director of Business Operations', photo: `${PHOTO_BASE}Valen-Murdock.png`, tier: 'director' },
+  { name: 'Eric Tipton', title: 'Director of Sales Operations', photo: `${PHOTO_BASE}Eric-Tipton.png`, tier: 'director' },
+  { name: 'Christine Hensley', title: 'Sr. Manager of Business Development', photo: `${PHOTO_BASE}Christine-Hensley.png`, tier: 'manager' },
+  { name: 'Tyler Chesnicka', title: 'Senior Manager of Business Development', photo: `${PHOTO_BASE}Tyler-Chesnicka.png`, tier: 'manager' },
+  { name: 'Savannah Clarkson', title: 'Senior Account Operations Manager', photo: `${PHOTO_BASE}Savannah-Clarkson.png`, tier: 'manager' },
+  { name: 'Ariel Vines', title: 'Client Operations Manager', photo: `${PHOTO_BASE}Ariel-Vines.png`, tier: 'manager' },
+  { name: 'Jasmine Castellanos Jones', title: 'Account Operations Manager', photo: `${PHOTO_BASE}Jasmine-Castellanos-Jones.png`, tier: 'manager' },
+  { name: 'Andrew Rocha', title: 'Digital Media Campaign Manager', photo: `${PHOTO_BASE}Andrew-Rocha.png`, tier: 'campaign' },
+  { name: 'Annie Johnson', title: 'Digital Media Campaign Manager', photo: `${PHOTO_BASE}Annie-Johnson.png`, tier: 'campaign' },
+  { name: 'Hayley Downing', title: 'Campaign Manager', photo: `${PHOTO_BASE}Hayley-Gorostieta.png`, tier: 'campaign' },
+  { name: 'Ivy Alves', title: 'Campaign Manager', photo: `${PHOTO_BASE}Ivy-Alves.png`, tier: 'campaign' },
+  { name: 'Katrina Sevilla', title: 'Campaign Manager', photo: `${PHOTO_BASE}Katrina-Sevilla.png`, tier: 'campaign' },
+  { name: 'Francisco Lopez', title: 'Campaign Coordinator', photo: `${PHOTO_BASE}Francisco-Lopez.png`, tier: 'campaign' },
+  { name: 'Bailey Davis', title: 'Account Operations Coordinator', photo: `${PHOTO_BASE}Bailey-Davis.png`, tier: 'campaign' },
+  { name: 'Shane Clancy', title: 'Developer', photo: `${PHOTO_BASE}Shane-Clancy.png`, tier: 'dev' },
+  { name: 'James Bell', title: 'Developer', photo: `${PHOTO_BASE}James-Bell.png`, tier: 'dev' },
+  { name: 'Trisha Garbett', title: 'Digital Account Administrator', photo: `${PHOTO_BASE}Trisha-Garbett.png`, tier: 'specialist' },
+  { name: 'Jeya Baylosis', title: 'Executive Partner', photo: `${PHOTO_BASE}Jeya-Baylosis.png`, tier: 'specialist' },
+  { name: 'Alejandro Giron', title: 'Marketer', photo: `${PHOTO_BASE}Alejandro-Giron.jpg`, tier: 'specialist' },
+  { name: 'Julianna DeSimone', title: 'Event Marketing Project Manager', photo: `${PHOTO_BASE}Julianna-DeSimone.png`, tier: 'specialist' },
+];
+
+export default team;
